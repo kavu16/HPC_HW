6ca563029f70b5f4bf5cac89bec9d249d0291e7f
Brady Edwards <bradyedwards@Bradys-Air-2.lan>
2023-03-06 00:04:26 -0500

    MMult pji

--- hw2/MMult1.cpp
+++ hw2/MMult1.cpp
@@ -26,9 +26,9 @@ void MMult0(long m, long n, long k, double *a, double *b, double *c) {
 
 void MMult1(long m, long n, long k, double *a, double *b, double *c) {
   // TODO: See instructions below
-  for (long i = 0; i < m; i++) {
+  for (long p = 0; p < k; p++) {
     for (long j = 0; j < n; j++) {
-      for (long p = 0; p < k; p++) {
+      for (long i = 0; i < m; i++) {
         double A_ip = a[i+p*m];
         double B_pj = b[p+j*k];
         double C_ij = c[i+j*m];
