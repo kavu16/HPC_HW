8db61835b66e38d6169b9f064782bb95a917861f
Brady Edwards <bse4289@nyu.edu>
2023-04-01 12:47:29 -0400

    built-in omp scan implementation

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -2,24 +2,38 @@
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
+#include <climits>
 
 // Scan A array and write result into prefix_sum array;
 // use long data type to avoid overflow
 void scan_seq(long* prefix_sum, const long* A, long n) {
   if (n == 0) return;
-  prefix_sum[0] = 0;
+  prefix_sum[0] = A[0];
   for (long i = 1; i < n; i++) {
-    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    prefix_sum[i] = prefix_sum[i-1] + A[i];
   }
+  printf("Final sum = %d\n", prefix_sum[n-1]);
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
-  int p = omp_get_num_threads();
+  int p = omp_get_max_threads();
   int t = omp_get_thread_num();
+  omp_set_num_threads(p);
   // Fill out parallel scan: One way to do this is array into p chunks
   // Do a scan in parallel on each chunk, then share/compute the offset
   // through a shared vector and update each chunk by adding the offset
   // in parallel
+  if (n==0) return;
+  prefix_sum[0] = 0;
+  long x = 0;
+  #pragma omp parallel for simd reduction(inscan,+: x)
+  for (long i = 0; i < n; i++) {
+    // prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    x += A[i];
+    #pragma omp scan inclusive(x)
+    prefix_sum[i] = x;
+  }
+  printf("Final sum = %d\n", prefix_sum[n-1]);
 }
 
 int main() {
@@ -29,7 +43,7 @@ int main() {
   long* B1 = (long*) malloc(N * sizeof(long));
   for (long i = 0; i < N; i++) A[i] = rand();
   for (long i = 0; i < N; i++) B1[i] = 0;
-  
+
   double tt = omp_get_wtime();
   scan_seq(B0, A, N);
   printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
@@ -40,6 +54,7 @@ int main() {
 
   long err = 0;
   for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+  long err_sum = 0;
   printf("error = %ld\n", err);
 
   free(A);
