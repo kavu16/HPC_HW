35931d131650fbd8ec41b31394bddcc4c931a2c4
Brady Edwards <bse4289@nyu.edu>
2023-04-03 03:39:38 -0400

    #include conditional edit

--- hw3/jacobi2D-omp.cpp
+++ hw3/jacobi2D-omp.cpp
@@ -1,8 +1,10 @@
 #include <vector>
-#include <omp.h>
 #include <cmath>
 #include <cstdio>
 #include <iostream>
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
 
 void jacobi_omp(std::vector<double>& u, const std::vector<double> f, const double n) {
 
@@ -48,6 +50,47 @@ void jacobi_omp(std::vector<double>& u, const std::vector<double> f, const doubl
     }
 }
 
+void jacobi(std::vector<double>& u, const std::vector<double> f, const double n) {
+
+    omp_set_num_threads(omp_get_max_threads());
+    double h = 1/(n+1);
+
+    double res = 0;
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 1; j < n + 1; j++) {
+            int index = i*(n+1) + j + 1;
+            double curr = (1/(h*h))*(2*u[index] - u[index - 1] - u[index + 1]) - f[index];
+            res += curr*curr;
+        }
+    }
+    res = sqrt(res);
+    printf("Initial res = %f\n", res);
+
+    int iter = 0;
+    while(iter < 5000) {
+        std::vector<double> unew (u);
+        for (int i=1; i < n + 1; i++) {
+            for (int j=1; j < n + 1; j++) {
+                int index = i*(n+1) + j + 1;
+                unew[index] = 0.25*(h*h*f[index] + u[index - 1] + u[index+1] + u[index - (n+1)] + u[index + (n+1)]);
+            }
+        }
+        
+        u.swap(unew);
+
+        res = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                int index = i*(n+1) + j + 1;
+                res += pow((1/(h*h))*(2*u[index] - u[index - 1] - u[index + 1]) - f[index],2);
+            }
+        }
+        res = sqrt(res);
+        if (iter%500 == 0) printf("Residual = %f\n", res);
+        ++iter;
+    }
+}
+
 int main() {
     int N = 1000;
 
@@ -56,7 +99,12 @@ int main() {
     std::fill(u.begin(),u.end(),0);
     std::fill(f.begin(),f.end(),1);
 
-    jacobi_omp(u, f, N);
+    
+    #if defined(_OPENMP)
+        jacobi_omp(u,f,N);
+    #else 
+        jacobi(u, f, N);
+    #endif
 
     return 0;
 }
