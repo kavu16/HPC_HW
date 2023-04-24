c5e79a38487920c10178f9f52d62b6a61c27c399
Brady Edwards <bse4289@nyu.edu>
2023-04-24 13:14:04 -0400

    mpi_scan init

---
+++ hw4/mpi_scan.cpp
@@ -0,0 +1,52 @@
+// MPI scan function
+// HPC HW4 Q3
+
+#include <mpi.h>
+#include <iostream>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    int mpirank, mpisize;
+    MPI_Comm_rank(comm, &mpirank);
+    MPI_Comm_size(comm, &mpisize);
+    
+    long N = atol(argv[1]);
+
+    int* local_scan = (double*) malloc(N/mpisize * sizeof(double));
+    if (mpirank == 0) {
+        double* scan_array = (double*) malloc(N * sizeof(double));
+        for (long i = 0; i < N; ++i) scan_array[i] = rand()*2;
+
+        MPI_Scatter(scan_array, N/mpisize, MPI_DOUBLE, &local_scan, N/mpisize, MPI_DOUBLE, 0, comm);
+    }
+
+    MPI_Barrier(comm);
+    double offset;
+    for (long i = 1; i < N/mpisize; ++i) {
+        local_scan[i] += local_scan[i-1];
+        offset = local_scan[i];
+    }
+
+    double* all_offsets = (double*) malloc(mpisize*sizeof(double));
+    MPI_Allgather(&offset, 1, MPI_DOUBLE, all_offsets, mpisize, MPI_DOUBLE, comm);
+
+    if (mpirank != 0) {
+        for (long i = 0; i < N/mpisize; ++i) {
+            for (long j = 0; j < mpirank; ++j) {
+                local_scan[i] += all_offsets[j];
+            }
+        }
+    }
+
+    MPI_Gather(local_scan, N/mpisize, MPI_DOUBLE, scan_array, N, MPI_DOUBLE, 0, comm);
+    if (mpirank == 0) {
+        std::cout << "Final scan = " << scan_array[N-1] << std::endl;
+    }
+    if (mpirank == 0) free(scan_array);
+    free(local_scan);
+    free(all_offsets);
+    return 0;
+}
